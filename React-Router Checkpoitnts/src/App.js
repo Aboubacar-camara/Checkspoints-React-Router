2ac6be771664774d9de866bb9ccab926c1097daf
Aboubacar Camara <119082495+Aboubacar-camara@users.noreply.github.com>
2023-05-15 13:40:22 +0000

    Add files via upload

---
+++ React-Router Checkpoitnts/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import MovieList from "./components/MovieList";
+import MovieDetail from "./components/MovieDetail";
+import {Routes, Route} from 'react-router-dom'
+// import MovieCard from "./components/MovieCard";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<MovieList />}/>
+        <Route path="/movie/:id" element={ <MovieDetail />}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
